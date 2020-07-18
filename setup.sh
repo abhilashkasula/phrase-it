3c1a6574778804e094a7abc4367eb99b6d3ec182
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-07-18 19:29:56 +0530

    | #7 | Added gitcommit template and setup.sh to add template and githooks
      GitHooks: precommit and prepush

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+git config --local commit.template ./git_commit.template
+
+cat <<EOF > .git/hooks/pre-commit  
+npm run allTests
+if [ \$? != 0 ]; then 
+    exit 1
+fi
+EOF
+
+chmod +x .git/hooks/pre-commit
+
+cat <<EOF > .git/hooks/pre-push  
+npm run allTests 
+if [ \$? != 0 ]; then 
+    exit 1
+fi
+EOF
+
+chmod +x .git/hooks/pre-push  
