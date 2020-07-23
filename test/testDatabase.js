a36d5c0697394126e3578dbe8a32a9ebb28f6d89
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-07-23 20:13:01 +0530

    | #10 | Abhilash/Anil | Updated tests

--- test/testDatabase.js
+++ test/testDatabase.js
@@ -2,137 +2,139 @@ const assert = require('assert');
 const Database = require('../src/database');
 
 describe('Unit Test', () => {
-  describe('get', () => {
-    it('should reject when there is an error', async () => {
-      const db = {get: (query, cb) => cb({err: 'error'})};
-      const database = new Database(db);
-      await assert.rejects(() => database.get('query'));
+  describe('Database', () => {
+    describe('get', () => {
+      it('should reject when there is an error', async () => {
+        const db = {get: (query, cb) => cb({err: 'error'})};
+        const database = new Database(db);
+        await assert.rejects(() => database.get('query'));
+      });
+
+      it('should resolve the row if there is no error', (done) => {
+        const db = {get: (query, cb) => cb(null, {id: 1})};
+        const database = new Database(db);
+        database
+          .get('query')
+          .then((row) => {
+            assert.deepStrictEqual(row, {id: 1});
+            done();
+          })
+          .catch((err) => done(err));
+      });
     });
 
-    it('should resolve the row if there is no error', (done) => {
-      const db = {get: (query, cb) => cb(null, {id: 1})};
-      const database = new Database(db);
-      database
-        .get('query')
-        .then((row) => {
-          assert.deepStrictEqual(row, {id: 1});
-          done();
-        })
-        .catch((err) => done(err));
-    });
-  });
-
-  describe('exec', () => {
-    it('should reject when there is an error', async () => {
-      const db = {exec: (query, cb) => cb({err: 'error'})};
-      const database = new Database(db);
-      await assert.rejects(() => database.exec('query'));
-    });
-    it('should resolve if there is no error', async () => {
-      const db = {exec: (query, cb) => cb(null)};
-      const database = new Database(db);
-      await assert.doesNotReject(() => database.exec('query'));
-    });
-  });
-
-  describe('createStory', () => {
-    it('should insert story and resolve story id 1 for first story', (done) => {
-      const db = {
-        get: (query, cb) => cb(null, undefined),
-        exec: (query, cb) => cb(null),
-      };
-      const database = new Database(db);
-      database
-        .createStory('John')
-        .then((id) => {
-          assert.strictEqual(id, 1);
-          done();
-        })
-        .catch((err) => done(err));
+    describe('exec', () => {
+      it('should reject when there is an error', async () => {
+        const db = {exec: (query, cb) => cb({err: 'error'})};
+        const database = new Database(db);
+        await assert.rejects(() => database.exec('query'));
+      });
+      it('should resolve if there is no error', async () => {
+        const db = {exec: (query, cb) => cb(null)};
+        const database = new Database(db);
+        await assert.doesNotReject(() => database.exec('query'));
+      });
     });
 
-    it('should insert story and resolve story id for story present', (done) => {
-      const db = {
-        get: (query, cb) => cb(null, {id: 1}),
-        exec: (query, cb) => cb(null),
-      };
-      const database = new Database(db);
-      database
-        .createStory('John')
-        .then((id) => {
-          assert.strictEqual(id, 2);
-          done();
-        })
-        .catch((err) => done(err));
+    describe('createStory', () => {
+      it('should insert story and resolve story id for first story', (done) => {
+        const db = {
+          get: (query, cb) => cb(null, undefined),
+          exec: (query, cb) => cb(null),
+        };
+        const database = new Database(db);
+        database
+          .createStory('John')
+          .then((id) => {
+            assert.strictEqual(id, 1);
+            done();
+          })
+          .catch((err) => done(err));
+      });
+
+      it('should insert story and resolve id if story is present', (done) => {
+        const db = {
+          get: (query, cb) => cb(null, {id: 1}),
+          exec: (query, cb) => cb(null),
+        };
+        const database = new Database(db);
+        database
+          .createStory('John')
+          .then((id) => {
+            assert.strictEqual(id, 2);
+            done();
+          })
+          .catch((err) => done(err));
+      });
+
+      it('should reject when there is an error in getting id', async () => {
+        const db = {
+          get: (query, cb) => cb({err: 'error'}),
+          exec: (query, cb) => cb(null),
+        };
+        const database = new Database(db);
+        await assert.rejects(() => database.createStory('John'));
+      });
+
+      it('should reject when there is error in inserting a story', async () => {
+        const db = {
+          get: (query, cb) => cb(null, {id: 1}),
+          exec: (query, cb) => cb({err: 'error'}),
+        };
+        const database = new Database(db);
+        await assert.rejects(() => database.createStory('John'));
+      });
     });
 
-    it('should reject when there is an error in getting id', async () => {
-      const db = {
-        get: (query, cb) => cb({err: 'error'}),
-        exec: (query, cb) => cb(null),
-      };
-      const database = new Database(db);
-      await assert.rejects(() => database.createStory('John'));
+    describe('updateStory', () => {
+      it('should update the story with given title and content', (done) => {
+        const db = {
+          get: (query, cb) => cb(null, {id: 1}),
+          exec: (query, cb) => cb(null),
+        };
+        const database = new Database(db);
+        database
+          .updateStory(1, 'Title', 'John', 'Content')
+          .then((result) => {
+            assert.deepStrictEqual(result, {status: 'updated'});
+            done();
+          })
+          .catch((err) => done(err));
+      });
+      it('should not update story and give error if id not found', (done) => {
+        const db = {
+          get: (query, cb) => cb(null, undefined),
+          exec: (query, cb) => cb(null),
+        };
+        const database = new Database(db);
+        database
+          .updateStory(1, 'Title', 'John', 'Content')
+          .then((result) => {
+            assert.deepStrictEqual(result, {error: 'unknown id'});
+            done();
+          })
+          .catch((err) => done(err));
+      });
     });
 
-    it('should reject when there is error in inserting a story', async () => {
-      const db = {
-        get: (query, cb) => cb(null, {id: 1}),
-        exec: (query, cb) => cb({err: 'error'}),
-      };
-      const database = new Database(db);
-      await assert.rejects(() => database.createStory('John'));
-    });
-  });
+    describe('getDrafts', () => {
+      it('should resolve all the drafts', (done) => {
+        const db = {all: (query, cb) => cb(null, [{id: 1}])};
+        const database = new Database(db);
+        database
+          .getDrafts()
+          .then((rows) => {
+            assert.deepStrictEqual(rows, [{id: 1}]);
+            done();
+          })
+          .catch((err) => done(err));
+      });
 
-  describe('updateStory', () => {
-    it('should update the story with given title and content', (done) => {
-      const db = {
-        get: (query, cb) => cb(null, {id: 1}),
-        exec: (query, cb) => cb(null),
-      };
-      const database = new Database(db);
-      database
-        .updateStory(1, 'Title', 'John', 'Content')
-        .then((result) => {
-          assert.deepStrictEqual(result, {status: 'updated'});
-          done();
-        })
-        .catch((err) => done(err));
-    });
-    it('should not update the story and give error if id not found', (done) => {
-      const db = {
-        get: (query, cb) => cb(null, undefined),
-        exec: (query, cb) => cb(null),
-      };
-      const database = new Database(db);
-      database
-        .updateStory(1, 'Title', 'John', 'Content')
-        .then((result) => {
-          assert.deepStrictEqual(result, {error: 'unknown id'});
-          done();
-        })
-        .catch((err) => done(err));
-    });
-  });
-
-  describe('getDrafts', () => {
-    it('should resolve all the drafts', (done) => {
-      const db = {all: (query, cb) => cb(null, [{id: 1}])};
-      const database = new Database(db);
-      database
-        .getDrafts()
-        .then((rows) => {
-          assert.deepStrictEqual(rows, [{id: 1}]);
-          done();
-        })
-        .catch((err) => done(err));
-    });
-
-    it('should reject when there is an error', async () => {
-      const db = {all: (query, cb) => cb({err: 'error'})};
-      const database = new Database(db);
-      await assert.rejects(() => database.getDrafts());
+      it('should reject when there is an error', async () => {
+        const db = {all: (query, cb) => cb({err: 'error'})};
+        const database = new Database(db);
+        await assert.rejects(() => database.getDrafts());
+      });
     });
   });
 });
--- test/testHandlers.js
+++ test/testHandlers.js
@@ -1,67 +1,71 @@
 const request = require('supertest');
 const {app} = require('../src/app');
 
-describe('newStory', () => {
-  it('should get editor html', (done) => {
-    request(app)
-      .get('/newStory')
-      .expect(200)
-      .expect('Content-Type', /html/)
-      .expect(/editor/, done);
-  });
-});
+describe('Integration tests', () => {
+  describe('Handlers', () => {
+    describe('newStory', () => {
+      it('should get editor html', (done) => {
+        request(app)
+          .get('/newStory')
+          .expect(200)
+          .expect('Content-Type', /html/)
+          .expect(/editor/, done);
+      });
+    });
 
-describe('createStory', () => {
-  it('should create a story and give back the story id', (done) => {
-    request(app)
-      .get('/createStory')
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .expect({id: 5}, done);
-  });
+    describe('createStory', () => {
+      it('should create a story and give back the story id', (done) => {
+        request(app)
+          .get('/createStory')
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .expect({id: 5}, done);
+      });
 
-  it('should create a story with incremented id', (done) => {
-    request(app)
-      .get('/createStory')
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .expect({id: 6}, done);
-  });
-});
+      it('should create a story with incremented id', (done) => {
+        request(app)
+          .get('/createStory')
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .expect({id: 6}, done);
+      });
+    });
 
-describe('updateStory', () => {
-  it('should update a story title, content for given story id', (done) => {
-    const block = {
-      type: 'paragraph',
-      data: {
-        text: 'A small paragraph',
-      },
-    };
-    const data = {id: 1, title: 'A new app', blocks: [block]};
-    request(app)
-      .post('/updateStory')
-      .send(data)
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .expect({status: 'updated'}, done);
-  });
+    describe('updateStory', () => {
+      it('should update a story title, content for given story id', (done) => {
+        const block = {
+          type: 'paragraph',
+          data: {
+            text: 'A small paragraph',
+          },
+        };
+        const data = {id: 1, title: 'A new app', blocks: [block]};
+        request(app)
+          .post('/updateStory')
+          .send(data)
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .expect({status: 'updated'}, done);
+      });
 
-  it('should give error for updating story with unknown id', (done) => {
-    request(app)
-      .post('/updateStory')
-      .send({id: 100})
-      .expect(404)
-      .expect('Content-Type', /json/)
-      .expect({error: 'unknown id'}, done);
-  });
-});
+      it('should give error for updating story with unknown id', (done) => {
+        request(app)
+          .post('/updateStory')
+          .send({id: 100})
+          .expect(404)
+          .expect('Content-Type', /json/)
+          .expect({error: 'unknown id'}, done);
+      });
+    });
 
-describe('getDrafts', () => {
-  it('should get all the drafts available', (done) => {
-    request(app)
-      .get('/drafts')
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .expect(/paragraph/, done);
+    describe('getDrafts', () => {
+      it('should get all the drafts available', (done) => {
+        request(app)
+          .get('/drafts')
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .expect(/paragraph/, done);
+      });
+    });
   });
 });
