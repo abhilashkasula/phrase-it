0e3117937696ea8ce2efe4c3a6080e68301caba3
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-07-23 19:52:38 +0530

    | #10 | Abhilash/Anil | Added tests for Dtatabase's updateStory method

--- test/testDatabase.js
+++ test/testDatabase.js
@@ -66,4 +66,29 @@ describe('Unit Test', () => {
         .catch((err) => done(err));
     });
   });
+
+  describe('updateStory', () => {
+    it('should update the story with given title and content', (done) => {
+      const db = {
+        get: (query, cb) => cb(null, {id: 1}),
+        exec: (query, cb) => cb(null),
+      };
+      const database = new Database(db);
+      database.updateStory(1, 'Title', 'John', 'Content').then((result) => {
+        assert.deepStrictEqual(result, {status: 'updated'});
+        done();
+      }).catch((err) => done(err));
+    });
+    it('should not update the story and give error if id not found', (done) => {
+      const db = {
+        get: (query, cb) => cb(null, undefined),
+        exec: (query, cb) => cb(null),
+      };
+      const database = new Database(db);
+      database.updateStory(1, 'Title', 'John', 'Content').then((result) => {
+        assert.deepStrictEqual(result, {error: 'unknown id'});
+        done();
+      }).catch((err) => done(err));
+    });
+  });
 });
