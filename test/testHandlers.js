21d151064eaac9525ece81a80da6374695efc214
Rashmi Gupta <rashmi224141@gmail.com>
2020-07-27 12:38:25 +0530

    | #12 | Rashmi/Nagendra | Added tests for getUserDetails handler
    added test for user name is absent
    
    What changes you have made?
    .....
    
    Why this change is necessary?
    ....
    
    What is the impact of the change?
    .....

--- test/testHandlers.js
+++ test/testHandlers.js
@@ -1,14 +1,14 @@
 const request = require('supertest');
 const https = require('https');
 const sinon = require('sinon');
-const { app } = require('../src/app');
+const {app} = require('../src/app');
 
 describe('Integration tests', () => {
   describe('Handlers', () => {
     describe('unauthorized user', () => {
       before(() => {
         app.set('sessionMiddleware', (req, res, next) => {
-          req.session = { isNew: true };
+          req.session = {isNew: true};
           next();
         });
       });
@@ -40,7 +40,7 @@ describe('Integration tests', () => {
       describe('newStory', () => {
         before(() => {
           app.set('sessionMiddleware', (req, res, next) => {
-            req.session = { isNew: false, id: 58025056 };
+            req.session = {isNew: false, id: 58025056};
             next();
           });
         });
@@ -57,7 +57,7 @@ describe('Integration tests', () => {
       describe('updateStory', () => {
         before(() => {
           app.set('sessionMiddleware', (req, res, next) => {
-            req.session = { isNew: false, id: 58025056 };
+            req.session = {isNew: false, id: 58025056};
             next();
           });
         });
@@ -69,13 +69,13 @@ describe('Integration tests', () => {
               text: 'A small paragraph',
             },
           };
-          const data = { id: 2, title: 'A new app', blocks: [block] };
+          const data = {id: 2, title: 'A new app', blocks: [block]};
           request(app)
             .post('/updateStory')
             .send(data)
             .expect(200)
             .expect('Content-Type', /json/)
-            .expect({ status: 'updated' }, done);
+            .expect({status: 'updated'}, done);
         });
 
         it('should give 404 for updating story with unknown id', (done) => {
@@ -84,7 +84,7 @@ describe('Integration tests', () => {
             .send({id: 100, title: 'Title', blocks: ''})
             .expect(404)
             .expect('Content-Type', /json/)
-            .expect({ error: 'unknown id' }, done);
+            .expect({error: 'unknown id'}, done);
         });
 
         it('should create story & give back id if id not present', (done) => {
@@ -96,7 +96,7 @@ describe('Integration tests', () => {
           };
           request(app)
             .post('/updateStory')
-            .send({ title: 'New blog', blocks: [block] })
+            .send({title: 'New blog', blocks: [block]})
             .expect(200)
             .expect('Content-Type', /json/, done);
         });
@@ -104,7 +104,7 @@ describe('Integration tests', () => {
       describe('stories', () => {
         before(() => {
           app.set('sessionMiddleware', (req, res, next) => {
-            req.session = { isNew: false, id: 58025056 };
+            req.session = {isNew: false, id: 58025056};
             next();
           });
         });
@@ -119,39 +119,39 @@ describe('Integration tests', () => {
 
         it('should give unknown user id for user not found', (done) => {
           app.set('sessionMiddleware', (req, res, next) => {
-            req.session = { isNew: false, id: 10000 };
+            req.session = {isNew: false, id: 10000};
             next();
           });
           request(app)
             .get('/stories')
             .expect(401)
             .expect('Content-Type', /json/)
-            .expect({ error: 'unknown id' }, done);
+            .expect({error: 'unknown id'}, done);
         });
       });
 
       describe('publish', () => {
         before(() => {
           app.set('sessionMiddleware', (req, res, next) => {
-            req.session = { isNew: false, id: 58025056 };
+            req.session = {isNew: false, id: 58025056};
             next();
           });
         });
         it('should publish a story present', (done) => {
           request(app)
             .post('/publish')
-            .send({ id: 2 })
+            .send({id: 2})
             .expect(200)
             .expect('Content-Type', /json/)
-            .expect({ status: 'published' }, done);
+            .expect({status: 'published'}, done);
         });
         it('should give No draft found for no draft', (done) => {
           request(app)
             .post('/publish')
-            .send({ id: 2 })
+            .send({id: 2})
             .expect(400)
             .expect('Content-Type', /json/)
-            .expect({ error: 'No draft found' }, done);
+            .expect({error: 'No draft found'}, done);
         });
         it('should not publish a story with no content and title', (done) => {
           request(app)
@@ -173,7 +173,7 @@ describe('Integration tests', () => {
         });
         it('should give available options if the user is auth', (done) => {
           app.set('sessionMiddleware', (req, res, next) => {
-            req.session = { isNew: false, id: 58025056 };
+            req.session = {isNew: false, id: 58025056};
             next();
           });
           request(app)
@@ -193,7 +193,7 @@ describe('Integration tests', () => {
 describe('handleHomePage', () => {
   it('should get index page if session not exists', (done) => {
     app.set('sessionMiddleware', (req, res, next) => {
-      req.session = { isNew: true };
+      req.session = {isNew: true};
       next();
     });
     request(app)
@@ -205,7 +205,7 @@ describe('handleHomePage', () => {
 
   it('should get welcome page if session not exists', (done) => {
     app.set('sessionMiddleware', (req, res, next) => {
-      req.session = { isNew: false, id: 56071561 };
+      req.session = {isNew: false, id: 56071561};
       next();
     });
     request(app)
@@ -217,19 +217,22 @@ describe('handleHomePage', () => {
 
   it('should give unknown user id for user not found', (done) => {
     app.set('sessionMiddleware', (req, res, next) => {
-      req.session = { isNew: false, id: 10 };
+      req.session = {isNew: false, id: 10};
       next();
     });
     request(app)
       .get('/')
       .expect(401)
       .expect('Content-Type', /json/)
-      .expect({ error: 'unknown id' }, done);
+      .expect({error: 'unknown id'}, done);
   });
 });
 
 describe('getUserDetails', () => {
-  before(() => {
+  afterEach(() => {
+    sinon.restore();
+  });
+  it('should redirect to / if there is no error', (done) => {
     const details = {
       access_token: 1234,
       id: 123,
@@ -241,15 +244,9 @@ describe('getUserDetails', () => {
       on: (event, callback) => callback(JSON.stringify(details)),
     };
     sinon.replace(https, 'request', (options, cb) => cb(res));
-  });
 
-  after(function() {
-    sinon.restore();
-  });
-
-  it('should get user details if there is no error', (done) => {
     app.set('sessionMiddleware', (req, res, next) => {
-      req.query = { code: 'somecode' };
+      req.query = {code: 'somecode'};
       req.session = {};
       next();
     });
@@ -257,6 +254,17 @@ describe('getUserDetails', () => {
   });
 
   it('should redirect to / if query not exists', (done) => {
+    const details = {
+      access_token: 1234,
+      id: 123,
+      name: 'name',
+      login: 'login',
+      avatar_url: 'avatar',
+    };
+    const res = {
+      on: (event, callback) => callback(JSON.stringify(details)),
+    };
+    sinon.replace(https, 'request', (options, cb) => cb(res));
     app.set('sessionMiddleware', (req, res, next) => {
       req.session = {};
       next();
@@ -264,19 +272,68 @@ describe('getUserDetails', () => {
     request(app).get('/user').expect(302).expect('location', '/', done);
   });
 
-  it('should get error if session not exists', (done) => {
+  it('should give error if session not exists', (done) => {
+    const details = {
+      access_token: 1234,
+      id: 123,
+      name: 'name',
+      login: 'login',
+      avatar_url: 'avatar',
+    };
+    const res = {
+      on: (event, callback) => callback(JSON.stringify(details)),
+    };
+    sinon.replace(https, 'request', (options, cb) => cb(res));
     app.set('sessionMiddleware', (req, res, next) => {
-      req.query = { code: 'somecode' };
+      req.query = {code: 'somecode'};
       next();
     });
     request(app).get('/user').expect(404, done);
+  });
+
+  it('should give error if user data is not present', (done) => {
+    const details = {
+      error: 'Err',
+    };
+    const res = {
+      on: (event, callback) => callback(JSON.stringify(details)),
+    };
+    sinon.replace(https, 'request', (options, cb) => cb(res));
+
+    app.set('sessionMiddleware', (req, res, next) => {
+      req.query = {code: 'somecode'};
+      req.session = {};
+      next();
+    });
+    request(app).get('/user').expect(404, done);
+  });
+
+  it('should redirect to / if user name is absent', (done) => {
+    const details = {
+      access_token: 1234,
+      id: 123,
+      name: null,
+      login: 'login',
+      avatar_url: 'avatar',
+    };
+    const res = {
+      on: (event, callback) => callback(JSON.stringify(details)),
+    };
+    sinon.replace(https, 'request', (options, cb) => cb(res));
+
+    app.set('sessionMiddleware', (req, res, next) => {
+      req.query = {code: 'somecode'};
+      req.session = {};
+      next();
+    });
+    request(app).get('/user').expect(302).expect('location', '/', done);
   });
 });
 
 describe('getPublishedStories', () => {
   it('should not give stories when user is not authorized', (done) => {
     app.set('sessionMiddleware', (req, res, next) => {
-      req.session = { isNew: true };
+      req.session = {isNew: true};
       next();
     });
     request(app)
@@ -287,12 +344,11 @@ describe('getPublishedStories', () => {
 
   it('should give stories when there is no error', (done) => {
     app.set('sessionMiddleware', (req, res, next) => {
-      req.session = { isNew: false, id: 1 };
+      req.session = {isNew: false, id: 1};
       next();
     });
     request(app).get('/publishedStories').expect(200).expect(/title/, done);
   });
-
 });
 
 describe('hasFields', () => {
