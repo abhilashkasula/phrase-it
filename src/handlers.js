7ffafd0085aba63a0748b64fadc3f44f9dc52974
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-07-29 18:41:28 +0530

    | #31 | Abhilash/Nagendra | Extracted options out of handlers

--- src/handlers.js
+++ src/handlers.js
@@ -1,24 +1,6 @@
 const {request} = require('./request');
 const statusCodes = require('./statusCodes');
-
-const getDetailsOptions = (token) => ({
-  host: 'api.github.com',
-  path: '/user',
-  method: 'GET',
-  headers: {
-    'User-Agent': 'curl/7.64.1',
-    Authorization: `token ${token}`,
-  },
-});
-
-const getTokenOptions = (query) => ({
-  host: 'github.com',
-  path: `/login/oauth/access_token?${query}`,
-  method: 'POST',
-  headers: {
-    Accept: 'application/json',
-  },
-});
+const {getDetailsOptions, getTokenOptions} = require('./options');
 
 const requestUserDetails = (req, res, token) => {
   const detailsOptions = getDetailsOptions(token);
