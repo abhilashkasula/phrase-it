a8e5d0eda805ad72875c7aa9366f648c24a1fb66
Anil Muraleedharan <56071561+anil-muraleedharan@users.noreply.github.com>
2020-08-10 20:32:19 +0530

    | #43 | Rashmi/Anil | Added a getDraft method in knexDatabase and its tests

--- src/knexDatabase.js
+++ src/knexDatabase.js
@@ -1,11 +1,21 @@
 class Database {
   constructor(db) {
     this.users = db('users').select();
+    this.stories = db('stories').select();
   }
 
   async addUser({id, name, avatar_url}) {
     await this.users.clone().insert({id, username: name, avatar_url});
     return {status: 'Added user'};
+  }
+
+  async getDraft(draftId, authorId) {
+    const conditions = {id: draftId, created_by: authorId, is_published: 0};
+    const [draft] = await this.stories.clone().select().where(conditions);
+    if (!draft) {
+      throw {error: 'No draft found'};
+    }
+    return draft;
   }
 }
 
--- test/testKnexDatabase.js
+++ test/testKnexDatabase.js
@@ -8,16 +8,50 @@ const knexInstance = knex(knexConfig.test);
 const db = new Database(knexInstance);
 
 describe('knexDatabase', () => {
-  beforeEach(async () => await resetKnexDB(knexInstance));
+  describe('addUser', () => {
+    beforeEach(async () => await resetKnexDB(knexInstance));
 
-  const userData = {id: 12345678, name: 'some name', avatar_url: 'url'};
-  it('should add a new user with given data', (done) => {
-    db.addUser(userData)
-      .then((res) => {
-        assert.deepStrictEqual(res, {status: 'Added user'});
-        done();
-      })
-      .catch((err) => done(err));
+    it('should add a new user with given data', (done) => {
+      const userData = {id: 12345678, name: 'some name', avatar_url: 'url'};
+      db.addUser(userData)
+        .then((res) => {
+          assert.deepStrictEqual(res, {status: 'Added user'});
+          done();
+        })
+        .catch((err) => done(err));
+    });
+  });
+
+  describe('getDraft', () => {
+    beforeEach(async () => await resetKnexDB(knexInstance));
+
+    it('should give draft if the draftId is proper', (done) => {
+      const expected = {
+        id: 2,
+        title: '8 Ways to Build Virality into your Product',
+        created_by: 58025056,
+        content:
+          '[{"type":"paragraph","data":{"text":"Computer science student."}}]',
+        is_published: 0,
+        last_modified: '2020-08-10 14:50:18',
+      };
+
+      db.getDraft(2, 58025056)
+        .then((res) => {
+          assert.deepStrictEqual(res, expected);
+          done();
+        })
+        .catch((err) => done(err));
+    });
+
+    it('should give error if draftId is wrong', (done) => {
+      db.getDraft(10, 58025056)
+        .catch((err) => {
+          assert.deepStrictEqual(err, {error: 'No draft found'});
+          done();
+        })
+        .catch((err) => done(err));
+    });
   });
 
   after(async () => await knexInstance.destroy());
